13eecf2158716aa23ec0d1b489a0ca8c52ebe1a2
Andre Oschadlin <andreoschadlin@gmail.com>
2011-10-26 12:19:58 -0700

    initial creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSencha::Application.config.secret_token = '682a565a7d3675230b0eeb89982ec2435b32bea033a8445476e9c0bdb9812e6d7ace73dee219c6fc520c1118aab6352aee2daa896a9f15c03b042ecaeb64b257'
